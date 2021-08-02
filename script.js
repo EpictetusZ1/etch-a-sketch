21d48052472ea1b1888a7bdcb8a82e71b8cc23f7
J Heaton <jackrobertheaton@gmail.com>
2021-08-02 14:44:29 -0400

    Add slider to control grid layout - no logic

--- script.js
+++ script.js
@@ -12,10 +12,27 @@ function changeColor() {
     this.classList.add("colored")
 }
 
+function resetColor() {
+    allGrid.forEach(element => element.classList.remove("colored"))
+}
+
 const allGrid = document.querySelectorAll(".grid-element")
 
-allGrid.forEach(element => element.addEventListener("mouseover", changeColor, {
-    capture: true,
-    once: true
-    }
-))
+allGrid.forEach(element => element.addEventListener("mouseover", changeColor))
+
+const resetBtn = document.getElementById("reset")
+
+function changeBtnColor() {
+    this.style.backgroundColor = "#9ef6f2"
+    setTimeout(revertColor,200)
+}
+
+function revertColor() {
+    resetBtn.style.backgroundColor =  "#84CDCA"
+}
+resetBtn.addEventListener("click", resetColor)
+resetBtn.addEventListener("click", changeBtnColor)
+
+let gridSlider = document.getElementById("gridSet")
+
+gridSlider.addEventListener("change", )
