df6922fff3c6d45d005546635e87b74067dea1ae
J Heaton <jackrobertheaton@gmail.com>
2021-08-02 13:33:06 -0400

    Add change color on mouseover

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ for (let i = 0; i < gridShape; i++) {
     gridContainer.appendChild(gridElement).classList.add("grid-element")
 }
 
+function changeColor() {
+    this.classList.add("colored")
+}
 
+const allGrid = document.querySelectorAll(".grid-element")
+
+allGrid.forEach(element => element.addEventListener("mouseover", changeColor, {
+    capture: true,
+    once: true
+    }
+))
