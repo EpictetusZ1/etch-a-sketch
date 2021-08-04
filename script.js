96ed4b388d5a009a4b2969eb440a8c2ebcfe0efe
J Heaton <jackrobertheaton@gmail.com>
2021-08-04 11:45:19 -0400

    Add logic to slider

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const gridContainer = document.querySelector(".grid-container")
-let gridElement
+const gridContainer = document.getElementById("grid-container")
+const adjustSlider = document.getElementById("size-adjust")
+const resetBtn = document.getElementById("reset")
 
-const gridShape = 16 * 16
+let initSize = 16
 
-for (let i = 0; i < gridShape; i++) {
-    gridElement = document.createElement('div')
-    gridContainer.appendChild(gridElement).classList.add("grid-element")
+let allGrid
+let gridDiv
+let size = initSize
+let shape
+
+function displayGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    shape = size * size
+    for (let i = 0; i < shape; i++) {
+        gridDiv = document.createElement('div')
+        gridDiv.addEventListener("mouseover", changeColor)
+        gridContainer.appendChild(gridDiv).classList.add("grid-element")
+    }
+    allGrid = document.querySelectorAll(".grid-element")
 }
+
+displayGrid(size)
 
 function changeColor() {
     this.classList.add("colored")
@@ -16,12 +31,6 @@ function resetColor() {
     allGrid.forEach(element => element.classList.remove("colored"))
 }
 
-const allGrid = document.querySelectorAll(".grid-element")
-
-allGrid.forEach(element => element.addEventListener("mouseover", changeColor))
-
-const resetBtn = document.getElementById("reset")
-
 function changeBtnColor() {
     this.style.backgroundColor = "#9ef6f2"
     setTimeout(revertColor,200)
@@ -30,9 +39,20 @@ function changeBtnColor() {
 function revertColor() {
     resetBtn.style.backgroundColor =  "#84CDCA"
 }
+
+function removeGridElements() {
+    gridContainer.innerHTML = ""
+}
+
 resetBtn.addEventListener("click", resetColor)
 resetBtn.addEventListener("click", changeBtnColor)
 
-let gridSlider = document.getElementById("gridSet")
+adjustSlider.addEventListener("change", (e)  => updateGrid(e.target.value))
+adjustSlider.addEventListener("change", resetColor)
 
-gridSlider.addEventListener("change", )
+function updateGrid(newSize) {
+    removeGridElements()
+    console.log(newSize)
+    displayGrid(newSize)
+}
+
